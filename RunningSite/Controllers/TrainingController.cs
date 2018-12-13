257153a028031924a2a224f2c56bc32619318b69
Eoin <eoinom@gmail.com>
2018-12-13 11:07:34 +0000

    Added Training Controller and associated Views. Added extra Views to Home Controller for Privacy, Terms and Site Map pages. Updated associated links in _Layout View.

---
+++ RunningSite/Controllers/TrainingController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RunningSite.Controllers
+{
+    public class TrainingController : Controller
+    {
+        // GET: Training
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Plans()
+        {
+            return View();
+        }
+
+        public ActionResult Tips()
+        {
+            return View();
+        }
+
+        public ActionResult Nutrition()
+        {
+            return View();
+        }
+
+        public ActionResult Hydration()
+        {
+            return View();
+        }
+
+        public ActionResult Kit()
+        {
+            return View();
+        }
+
+        public ActionResult Raceday()
+        {
+            return View();
+        }
+    }
+}
